c9c42254b99647426c595daa650acd29f07231a8
Matt <matt@matts-mbp.home>
2017-04-02 13:50:21 +0100

    pokemon logged out once encountered

--- Assets/scripts/map/Area.cs
+++ Assets/scripts/map/Area.cs
@@ -7,15 +7,23 @@ public class Area : MonoBehaviour {
 	public PokemonName[] grassCommon;
 	public PokemonName[] grassUncommon;
 	public PokemonName[] grassRare;
+	public PokemonName[] grassVeryRare;
+	public PokemonName[] grassExtremelyRare;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public PokemonName GetWildGrassPokemon(PokemonRarity rarity) {
+		if (rarity == PokemonRarity.Uncommon && grassUncommon.Length > 0) {
+			return grassUncommon [Random.Range (0, grassUncommon.Length)];
+		}
+		if (rarity == PokemonRarity.Rare && grassRare.Length > 0) {
+			return grassRare [Random.Range (0, grassRare.Length)];
+		}
+		if (rarity == PokemonRarity.VeryRare && grassVeryRare.Length > 0) {
+			return grassVeryRare [Random.Range (0, grassVeryRare.Length)];
+		}
+		if (rarity == PokemonRarity.ExtremelyRare && grassExtremelyRare.Length > 0) {
+			return grassExtremelyRare [Random.Range (0, grassExtremelyRare.Length)];
+		}	
+		return grassCommon [Random.Range (0, grassCommon.Length)];
 	}
 }
 
--- Assets/scripts/misc/GameManager.cs
+++ Assets/scripts/misc/GameManager.cs
@@ -7,12 +7,11 @@ public class GameManager : MonoBehaviour {
 	public GameObject player;
 	public GameObject battle;
 	public Camera mainCamera;
-
-
+	public Area area;
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
@@ -22,6 +21,8 @@ public class GameManager : MonoBehaviour {
 
 	public void EnterWildBattle(Biomes biome, PokemonRarity rarity) {
 		Debug.Log ("Encounter" + biome + rarity);
+
+		PokemonName pokemonName = area.GetWildGrassPokemon (rarity);
 
 		battle.GetComponent<Battle> ().WildBattle (mainCamera);
 		player.GetComponent<PlayerMovement> ().SetMove (false);
