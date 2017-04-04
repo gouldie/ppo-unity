449b8be6cd1c72c01bfd3654b9939d55e601e0ba
Matt <mgould@breakingfreegroup.com>
2017-04-04 20:38:16 +0100

    updates

---
+++ Assets/scripts/map/GrassPatchHandler.cs
@@ -0,0 +1,38 @@
+﻿//Original Scripts by IIColour (IIColour_Spectrum)
+
+using UnityEngine;
+using System.Collections;
+
+public class GrassPatchHandler : MonoBehaviour {
+    private GameObject overlay;
+//    private AudioSource walkSound;
+//    public AudioClip walkClip;
+
+    void Awake() {
+        overlay = transform.FindChild("Overlay").gameObject;
+//        walkSound = transform.GetComponent<AudioSource>();
+    }
+
+    void Start() {
+        overlay.SetActive(false);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log(other);
+        if (other.tag == "Player") {
+            overlay.SetActive(true);
+
+
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        Debug.Log(other);
+        if (other.tag == "Player") {
+            overlay.SetActive(false);
+
+
+        }
+    }
+}
