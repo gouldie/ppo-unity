3198d86d45eecfe0f858a0eb4865b57d60636045
Matt <mgould@breakingfreegroup.com>
2017-07-11 21:13:51 +0100

    photon setup

---
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : Photon.PunBehaviour {
+
+    public byte MaxPlayersPerRoom = 4;
+    public GameObject controlPanel;
+    public GameObject progressLabel;
+
+    void Start() {
+        progressLabel.SetActive(false);
+        controlPanel.SetActive(true);
+    }
+
+    public override void OnConnectedToMaster() {
+        Debug.Log("Connected to master.");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnectedFromPhoton() {
+        progressLabel.SetActive(false);
+        controlPanel.SetActive(true);
+        
+        Debug.LogWarning("Disconnected from Photon.");
+    }
+
+    public override void OnPhotonRandomJoinFailed (object[] codeAndMsg) {
+        Debug.Log("Failed to join random room. Creating..");
+
+		// #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+        PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = MaxPlayersPerRoom }, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined room.");
+    }
+
+    public void connect() {
+        progressLabel.SetActive(true);
+        controlPanel.SetActive(false);
+
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+}
---
+++ Assets/Scripts/PlayerNameInputField.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// Player name input field. Let the user input his name, will appear above the player in the game.
+/// </summary>
+[RequireComponent(typeof(InputField))]
+public class PlayerNameInputField : MonoBehaviour {
+
+    // Store the PlayerPref Key to avoid typos
+    static string playerNamePrefKey = "PlayerName";
+
+    void Start() {
+        string defaultName = "";
+        InputField _inputField = this.GetComponent<InputField>();
+
+        if (_inputField != null)
+        {
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                _inputField.text = defaultName;
+            }
+        }
+
+
+        PhotonNetwork.playerName = defaultName;
+    }
+
+
+    public void SetPlayerName(string value) {
+        // force a trailing space string in case value is an empty string, else playerName would not be updated.
+        PhotonNetwork.playerName = value + " ";
+
+        PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
+}
